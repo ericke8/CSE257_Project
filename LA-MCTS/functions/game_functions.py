cd716574950b37375132f8f62a51b8855f1a3f50
eric <keric.yi@gmail.com>
2021-05-18 22:02:44 -0700

    add extra functions, graphing

--- LA-MCTS/functions/game_functions.py
+++ LA-MCTS/functions/game_functions.py
@@ -4,6 +4,7 @@ import json
 import os
 import highway_env
 import gym_maze
+from gym_minigrid.wrappers import *
 
 class Highway:
     
@@ -21,7 +22,7 @@ class Highway:
         self.policy_shape = (5, 5)
         
         #tunable hyper-parameters in LA-MCTS
-        self.Cp           = 1
+        self.Cp           = 10
         self.leaf_size    = 20
         self.kernel_type  = "rbf"
         self.gamma_type   = "scale"
@@ -179,6 +180,69 @@ class Maze:
                 inputs = (obs - self.mean)/self.std
                 
                 action = self.action_dict[np.argmax(np.dot(M, inputs))]
+                observations.append(obs)
+                actions.append(action)
+                obs, r, done, _ = self.env.step(action)
+                totalr += r
+                steps  += 1
+                if self.render:
+                    self.env.render()
+            returns.append(totalr)
+            
+        return np.mean(returns)*-1
+    
+    
+class MiniGrid:
+    
+    def __init__(self):
+        self.mean    = 0
+        self.std     = 1
+                
+        self.dims    = 21
+        self.lb      = -1 * np.ones(self.dims)
+        self.ub      =  1 * np.ones(self.dims)
+        self.counter = 0
+        self.env     = ImgObsWrapper(StateBonus(gym.make('MiniGrid-Empty-5x5-v0')))
+        self.num_rollouts = 3
+        self.render  = False
+        self.policy_shape = (7, 3)
+        
+        #tunable hyper-parameters in LA-MCTS
+        self.Cp           = 100
+        self.leaf_size    = 20
+        self.kernel_type  = "rbf"
+        self.gamma_type   = "scale"
+        self.ninits       = 30
+        
+        print("===========initialization===========")
+        print("mean:", self.mean)
+        print("std:", self.std)
+        print("dims:", self.dims)
+        print("policy:", self.policy_shape )
+            
+    def __call__(self, x):
+        self.counter += 1
+        assert len(x) == self.dims
+        assert x.ndim == 1
+        assert np.all(x <= self.ub) and np.all(x >= self.lb)
+        
+        M = x.reshape(self.policy_shape)
+        
+        returns = []
+        observations = []
+        actions = []
+        
+        for i in range(self.num_rollouts):
+            obs    = self.env.reset()
+            done   = False
+            totalr = 0.
+            steps  = 0
+            while not done:
+                # M      = self.policy
+                inputs = (obs - self.mean)/self.std
+                inputs = inputs.transpose(2,0,1).reshape(3,-1)
+                
+                action = np.argmax(np.sum(np.dot(M, inputs), axis=1))
                 observations.append(obs)
                 actions.append(action)
                 obs, r, done, _ = self.env.step(action)
--- LA-MCTS/functions/pybullet_functions.py
+++ LA-MCTS/functions/pybullet_functions.py
@@ -71,7 +71,7 @@ class Kuka:
     
     def __init__(self):
         self.mean    = 0
-        self.std     = 10
+        self.std     = 1
                 
         self.dims    = 27
         self.lb      = -1 * np.ones(self.dims)
@@ -132,7 +132,7 @@ class Thrower:
     
     def __init__(self):
         self.mean    = 0
-        self.std     = 10
+        self.std     = 1
                 
         self.dims    = 336
         self.lb      = -1 * np.ones(self.dims)
@@ -193,7 +193,7 @@ class Pusher:
     
     def __init__(self):
         self.mean    = 0
-        self.std     = 10
+        self.std     = 1
                 
         self.dims    = 385
         self.lb      = -1 * np.ones(self.dims)
@@ -254,7 +254,7 @@ class FlagRun:
     
     def __init__(self):
         self.mean    = 0
-        self.std     = 10
+        self.std     = 1
                 
         self.dims    = 748
         self.lb      = -1 * np.ones(self.dims)
@@ -269,6 +269,67 @@ class FlagRun:
         self.Cp           = 10
         self.leaf_size    = 100
         self.kernel_type  = "linear"
+        self.gamma_type   = "scale"
+        self.ninits       = 30
+        
+        print("===========initialization===========")
+        print("mean:", self.mean)
+        print("std:", self.std)
+        print("dims:", self.dims)
+        print("policy:", self.policy_shape )
+            
+    def __call__(self, x):
+        self.counter += 1
+        assert len(x) == self.dims
+        assert x.ndim == 1
+        assert np.all(x <= self.ub) and np.all(x >= self.lb)
+        
+        M = x.reshape(self.policy_shape)
+        
+        returns = []
+        observations = []
+        actions = []
+        
+        for i in range(self.num_rollouts):
+            obs    = self.env.reset()
+            done   = False
+            totalr = 0.
+            steps  = 0
+            while not done:
+                # M      = self.policy
+                inputs = (obs - self.mean)/self.std
+                
+                action = np.dot(M, inputs)
+                observations.append(obs)
+                actions.append(action)
+                obs, r, done, _ = self.env.step(action)
+                totalr += r
+                steps  += 1
+                if self.render:
+                    self.env.render()
+            returns.append(totalr)
+            
+        return np.mean(returns)*-1
+    
+class RaceCar:
+    
+    def __init__(self):
+        self.mean    = 0
+        self.std     = 1
+                
+        self.dims    = 4
+        self.lb      = -1 * np.ones(self.dims)
+        self.ub      =  1 * np.ones(self.dims)
+        self.counter = 0
+        self.env     = gym.make('RacecarBulletEnv-v0')
+        self.num_rollouts = 3
+        self.render  = False
+        self.policy_shape = (2, 2)
+        
+        #tunable hyper-parameters in LA-MCTS
+        self.Cp           = 10
+        self.leaf_size    = 20
+        self.kernel_type  = "rbf"
         self.gamma_type   = "scale"
         self.ninits       = 30
         
